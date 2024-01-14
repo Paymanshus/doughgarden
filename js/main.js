970bf7064cf1f65e7a718345b32b4387c515c2c7
Paymanshus <paymanshus@gmail.com>
2024-01-15 00:29:45 +0530

    Menu and contact add

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ $(function() {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+$.get("nav.html", function(data) {
+    $("#nav-placeholder").replaceWith(data);
+});
+
+// $.get("instabutton.html", function(data) {
+//     $("#instagram-button").load(data);
+// });
+
+$(function() {
+    $("#instagram-button").load("instabutton.html");
+});
